4329f884c0e3d3899ac10d1c10ffa15580d43bb8
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2022-01-30 22:45:23 +1100

    Add more tests for fetchContact

--- test/mailchimp/fetchContact.test.ts
+++ test/mailchimp/fetchContact.test.ts
@@ -1,23 +1,32 @@
-import fetchContacts from "../../src/mailchimp/fetchContact";
-import axios, { AxiosResponse } from "axios";
+import fetchContacts from '../../src/mailchimp/fetchContact';
+import { when } from "jest-when";
 
-jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock("../../src/mailchimp/fetchContact");
 
 describe("fetchContacts", () => {
-  test("returns data from beginning when fromStart=true", async () => {
+  it("returns data from beginning when fromStart=true", async () => {
     const config = {
       apiKey: "",
       host: "",
     };
     const response = require("./test-data.json");
+    when(fetchContacts).calledWith(true, config).mockResolvedValue(response);
 
-    mockedAxios.get.mockResolvedValueOnce(response);
+    return fetchContacts(true, config).then(result => {
+        expect(result.members).toHaveLength(10);
+    })
+  });
 
-    const result = await fetchContacts(true, config)
-    console.log(result, ">>>>>>>>>>>>>>>>>>>>");
+  it("returns data from last run when fromStart=false", async () => {
+    const config = {
+      apiKey: "",
+      host: "",
+    };
+    const response = { members: [{ id: "test" }] }
+    when(fetchContacts).calledWith(false, config).mockResolvedValue(response);
 
-    expect(mockedAxios.get).toHaveBeenCalled();
-    expect(result.members).toHaveLength(10);
+    return fetchContacts(true, config).then(result => {
+        expect(result.members).toHaveLength(10);
+    })
   });
 });
